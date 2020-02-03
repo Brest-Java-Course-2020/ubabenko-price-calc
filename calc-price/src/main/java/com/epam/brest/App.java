1690da04ff5e4863829f521738dd58fb8660fb3c
samyside <samyside@yandex.ru>
2020-02-03 13:19:40 +0300

    1st implemenation

--- calc-price/src/main/java/com/epam/brest/App.java
+++ calc-price/src/main/java/com/epam/brest/App.java
@@ -1,13 +1,59 @@
 package com.epam.brest;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+        Double[] enteredValues = new Double[4];
+
+        Scanner scanner = new Scanner(System.in);
+        String inputValue;
+        int i = 0;
+        do {
+            if (i == 0) {
+                System.out.println("Please, enter distance or Q for exit: ");
+            } else if (i == 1) {
+                System.out.println("Please, enter price per km or Q for exit: ");
+            } else if (i == 2) {
+                System.out.println("Please, enter weight or Q for exit: ");
+            } else {
+                System.out.println("Please, enter price per kg or Q for exit: ");
+            }
+
+            inputValue = scanner.next();
+
+            if (!isExitValue(inputValue)) {
+                if (isCorrectDoubleValue(inputValue)) {
+                    enteredValues[i] = Double.parseDouble(inputValue);
+                    i++;
+                }
+            }
+
+            if (i == 4) {
+                Double calcResult = enteredValues[0]*enteredValues[1] + enteredValues[2]*enteredValues[3];
+                System.out.println("Price: $" + calcResult);
+                i = 0;
+            }
+
+        } while (!isExitValue(inputValue));
+
+        System.out.println("Finish!");
+
     }
+
+    private static boolean isExitValue(String value) {
+        return value.equalsIgnoreCase("Q");
+    }
+
+    private static boolean isCorrectDoubleValue(String value) {
+        boolean checkResult;
+        try {
+            double enteredDoubleValue = Double.parseDouble(value);
+            checkResult = enteredDoubleValue >= 0;
+        } catch (NumberFormatException ex) {
+            checkResult = false;
+        }
+        return checkResult;
+    }
+
 }
