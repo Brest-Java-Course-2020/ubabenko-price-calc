60f5842c700d54a246e2150256ca948361e99518
samyside <samyside@yandex.ru>
2020-02-04 17:52:06 +0300

    parse from properties

--- calc-price/src/main/java/com/epam/brest/App.java
+++ calc-price/src/main/java/com/epam/brest/App.java
@@ -1,48 +1,78 @@
 package com.epam.brest;
 
+import java.io.*;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class App {
-    public static void main( String[] args ) {
-        Double[] enteredValues = new Double[4];
+    private static final int ENTER_DISTANCE = 0;
+    private static final int ENTER_WEIGHT = 1;
+    private static final String EXIT_VALUE = "Q";
+    private static double pricePerKm, pricePerKg;
 
+    static {
+        try {
+            FileInputStream configPrices = new FileInputStream("src/main/resources/prices.properties");
+            Properties prices = new Properties();
+            Reader reader = new InputStreamReader(configPrices);
+            prices.load(reader);
+            pricePerKm = Double.parseDouble(prices.getProperty("price_per_km"));
+            pricePerKg = Double.parseDouble(prices.getProperty("price_per_kg"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Error! Couldn't find file config");
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (IOException e) {
+            System.out.print("Error! Something goes wrong :D");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    public static void main( String[] args ) {
+        double distance=0.0, weight=0.0;
+        int idOperation = 0;
+
+        System.out.printf("System info:\npricePerKm= %f\npricePerKg= %f\n\n", pricePerKm, pricePerKg);
+        System.out.println("For exit use: Ctrl + C");
         Scanner scanner = new Scanner(System.in);
         String inputValue;
-        int i = 0;
         do {
-            if (i == 0) {
-                System.out.println("Please, enter distance or Q for exit: ");
-            } else if (i == 1) {
-                System.out.println("Please, enter price per km or Q for exit: ");
-            } else if (i == 2) {
-                System.out.println("Please, enter weight or Q for exit: ");
+            if (idOperation == ENTER_DISTANCE) {
+                System.out.print("Distance : ");
+                inputValue = scanner.next();
+                if (isExitValue(inputValue)) {
+                    if (isCorrectDoubleValue(inputValue)) {
+                        distance = Double.parseDouble(inputValue);
+                    }
+                }
             } else {
-                System.out.println("Please, enter price per kg or Q for exit: ");
-            }
-
-            inputValue = scanner.next();
-
-            if (!isExitValue(inputValue)) {
-                if (isCorrectDoubleValue(inputValue)) {
-                    enteredValues[i] = Double.parseDouble(inputValue);
-                    i++;
+                System.out.print("Weight : ");
+                inputValue = scanner.next();
+                if (isExitValue(inputValue)) {
+                    if (isCorrectDoubleValue(inputValue)) {
+                        weight = Double.parseDouble(inputValue);
+                    }
                 }
             }
 
-            if (i == 4) {
-                Double calcResult = enteredValues[0]*enteredValues[1] + enteredValues[2]*enteredValues[3];
-                System.out.println("Price: $" + calcResult);
-                i = 0;
+            if (idOperation == ENTER_WEIGHT) {
+                double calcResult = distance * pricePerKm + weight * pricePerKg;
+                System.out.printf("Price: %f $\n", calcResult);
+                idOperation = ENTER_DISTANCE;
+            } else {
+                idOperation++;
             }
-
-        } while (!isExitValue(inputValue));
+        } while (isExitValue(inputValue));
 
         System.out.println("Finish!");
-
     }
 
+    private static boolean checkValue(String value) {
+        return value.equalsIgnoreCase(EXIT_VALUE);
+    }
     private static boolean isExitValue(String value) {
-        return value.equalsIgnoreCase("Q");
+        return !value.equalsIgnoreCase("Q");
     }
 
     private static boolean isCorrectDoubleValue(String value) {
@@ -50,10 +80,9 @@ public class App {
         try {
             double enteredDoubleValue = Double.parseDouble(value);
             checkResult = enteredDoubleValue >= 0;
-        } catch (NumberFormatException ex) {
+        } catch (NumberFormatException e) {
             checkResult = false;
         }
         return checkResult;
     }
-
 }
